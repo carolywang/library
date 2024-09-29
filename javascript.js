11b9338cd56cf6173e5c473c0323377e07294c03
Carol Wang <110089439+ycarolwang@users.noreply.github.com>
2024-09-30 01:06:37 +0800

    add book card based on user input

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+let runningBookArr = [];
 
 // constructor
 function Book(title, author, pages, readStatus) {
@@ -10,6 +11,7 @@ function Book(title, author, pages, readStatus) {
 
 // take user's input and store the new book objects into an array
 function addBookToLibrary(newBook) {
+  runningBookArr.push(newBook);
   myLibrary.push(newBook);
 }
 
@@ -25,32 +27,58 @@ const book3 = new Book("Norwegian wood", "Haruki Murakami", 296, "read");
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
+console.log(runningBookArr);
 console.log(myLibrary);
 
 // function to loop through the array and display each book on the page
-function displayBooks() {
-  for (const book of myLibrary) {
-    // append card
-    const bookCard = document.createElement("div");
-    bookCard.className = "card";
-    document.getElementById("container").appendChild(bookCard);
-    // append book title to card
-    const title = document.createElement("p");
-    title.innerText = `Book: ${book.title}`;
-    bookCard.appendChild(title);
-    // append author name to card
-    const author = document.createElement("p");
-    author.innerText = `Author: ${book.author}`;
-    bookCard.appendChild(author);
-    // append number of pages to card
-    const pages = document.createElement("p");
-    pages.innerText = `Pages: ${book.pages}`;
-    bookCard.appendChild(pages);
-    // append read status to card
-    const readStatus = document.createElement("p");
-    readStatus.innerText = `Read status: ${book.readStatus}`;
-    bookCard.appendChild(readStatus);
-  }
+function displayBooks(book) {
+  // append card
+  const bookCard = document.createElement("div");
+  bookCard.className = "card";
+  document.getElementById("container").appendChild(bookCard);
+  // append book title to card
+  const title = document.createElement("p");
+  title.innerText = `Book: ${book.title}`;
+  bookCard.appendChild(title);
+  // append author name to card
+  const author = document.createElement("p");
+  author.innerText = `Author: ${book.author}`;
+  bookCard.appendChild(author);
+  // append number of pages to card
+  const pages = document.createElement("p");
+  pages.innerText = `Pages: ${book.pages}`;
+  bookCard.appendChild(pages);
+  // append read status to card
+  const readStatus = document.createElement("p");
+  readStatus.innerText = `Read status: ${book.readStatus}`;
+  bookCard.appendChild(readStatus);
 }
+myLibrary.map((book) => displayBooks(book));
 
-displayBooks();
+// add new book
+const addBookButton = document.querySelector("button");
+const dialog = document.querySelector("dialog");
+const closeButton = document.querySelector("dialog button");
+const submit = document.querySelector(".submit");
+// click add new book button to bring up a form
+addBookButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+// close button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+// submit book info and add card
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  runningBookArr = [];
+  let title = document.getElementById("book-title").value;
+  let author = document.getElementById("book-author").value;
+  let pages = document.getElementById("pages").value;
+  let readStatus = document.getElementById("read-status").value;
+  const newBook = new Book(title, author, pages, readStatus);
+  addBookToLibrary(newBook);
+  dialog.close();
+  document.getElementById("form").reset();
+  runningBookArr.map((book) => displayBooks(book));
+});
